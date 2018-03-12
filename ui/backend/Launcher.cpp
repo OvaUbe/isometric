ed3ad7829b4712fa4a040505ed209da345c28d86
ovaube <trevorismay@gmail.com>
2018-03-13 01:41:25 +0300

    ui/backend/Loader: Proxy backend interfaces to qml.

--- ui/backend/Launcher.cpp
+++ ui/backend/Launcher.cpp
@@ -2,18 +2,42 @@
 
 #include <QQmlContext>
 
+#include <functional>
+
 namespace ui {
+
+using namespace std::placeholders;
+
 
 Launcher::Launcher(QObject* parent)
     :   QObject(parent) { }
 
 
 Launcher::Launcher(const QQmlEngine& qmlEngine, const UiContextRef& uiContext)
-    :   _uiContext(uiContext) {
+    :   _uiContext(uiContext),
+        _isGameActive() {
     qmlEngine.rootContext()->setContextProperty("global_backend_launcher", this);
+
+    _launcherConnection = _uiContext->getApplication().getLauncher()->gameStatusChanged().connect(std::bind(&Self::OnGameStatusChanged, this, _1), _uiContext->getTaskQueue());
 }
 
 
-void Launcher::newGame() { }
+void Launcher::newGame() {
+    _uiContext->getApplication().getLauncher()->newGame();
+}
+
+
+void Launcher::quitGame() {
+    _uiContext->getApplication().getLauncher()->quitGame();
+}
+
+
+void Launcher::OnGameStatusChanged(bool status) {
+    if (_isGameActive == status)
+        return;
+
+    _isGameActive = status;
+    emit gameStatusChanged();
+}
 
 }
--- ui/backend/Launcher.h
+++ ui/backend/Launcher.h
@@ -7,16 +7,34 @@
 namespace ui {
 
 class Launcher : public QObject {
+    using Self = Launcher;
+
     Q_OBJECT
+    Q_PROPERTY(bool isGameActive READ isGameActive NOTIFY gameStatusChanged)
 
 private:
-    UiContextPtr _uiContext;
+    UiContextPtr    _uiContext;
+
+    bool            _isGameActive;
+
+    gum::Token      _launcherConnection;
 
 public:
     Launcher(QObject* parent = nullptr);
     Launcher(const QQmlEngine& qmlEngine, const UiContextRef& iuContext);
 
+    Q_INVOKABLE bool isGameActive() const {
+        return _isGameActive;
+    }
+
     Q_INVOKABLE void newGame();
+    Q_INVOKABLE void quitGame();
+
+signals:
+    void gameStatusChanged();
+
+private:
+    void OnGameStatusChanged(bool status);
 };
 
 }
