42e2f2d2a4cba121846154df635b9271ff50e405
ovaube <trevorismay@gmail.com>
2018-03-13 01:39:29 +0300

    Implement first application interface.

---
+++ igd/Application.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <igd/ILauncher.h>
+
+namespace igd {
+namespace app {
+
+struct Application {
+    struct Impl;
+    GUM_DECLARE_UNIQUE_REF(Impl);
+
+private:
+    ImplUniqueRef _impl;
+
+public:
+    Application();
+    ~Application();
+
+    ILauncherRef getLauncher() const;
+};
+
+}
+}
