c9881bda36513e4e3858cb8faee67805b9c7461f
ovaube <trevorismay@gmail.com>
2018-03-13 01:40:22 +0300

    ui/backend/UiContext: Create application.

--- ui/backend/UiContext.h
+++ ui/backend/UiContext.h
@@ -2,16 +2,24 @@
 
 #include <QTimer>
 
+#include <igd/Application.h>
+
 #include <gum/async/TaskQueue.h>
 
 namespace ui {
 
 class UiContext {
+    igd::app::Application   _application;
+
     QTimer                  _mainTick;
     gum::TaskQueueRef       _taskQueue;
 
 public:
     explicit UiContext();
+
+    const igd::app::Application& getApplication() const {
+        return _application;
+    }
 
     gum::ITaskQueueRef getTaskQueue() const {
         return _taskQueue;
