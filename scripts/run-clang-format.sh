990f8b433287fb4ee7780c93c3157c59786ad37b
ovaube <trevorismay@gmail.com>
2018-03-21 22:23:32 +0300

    run-clang-format.sh: Check for version.

--- scripts/run-clang-format.sh
+++ scripts/run-clang-format.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-find_clang_format() {
-    version=6.0
+clang_format_version=6.0
 
-    result=`which clang-format-${version}` && echo ${result} && exit 0
+do_find_clang_format() {
+    result=`which clang-format-${clang_format_version}` && echo ${result} && exit 0
     result=`which clang-format` && echo ${result} && exit 0
     exit 1
+}
+
+find_clang_format() {
+    clang_format=`do_find_clang_format` || exit 1
+
+    actual_version=`${clang_format} --version | grep -oP "version \K(\d\.\d)"`
+    if [[ ${actual_version} != ${clang_format_version} ]]; then exit 1; fi
+
+    echo ${clang_format}
+    exit 0
 }
 
 initial_state=`git status | grep "modified"`
@@ -15,7 +25,9 @@ if ! [[ -z $initial_state ]]; then
 fi
 
 clang_format=`find_clang_format`
-if [[ $? -ne 0 ]]; then echo "clang-format not found"; exit 0; fi
+if [[ $? -ne 0 ]]; then echo "clang-format ${clang_format_version} not found"; exit 0; fi
+echo "Found ${clang_format}"
+
 clang_format_opts="-style=file -assume-filename=.clang-format -i"
 
 src_dirs=(
