a91a9f317cb968312b9e1bc2578db19ccc1947b3
ovaube <trevorismay@gmail.com>
2018-03-25 13:02:02 +0300

    Move git stuff to clangformattest.
    More convinient for manual run-clang-format launching.

--- scripts/run-clang-format.sh
+++ scripts/run-clang-format.sh
@@ -18,12 +18,6 @@ find_clang_format() {
     exit 0
 }
 
-initial_state=`git status | grep "modified"`
-if ! [[ -z $initial_state ]]; then
-    echo "Clang-format script must be applied on a clean git state"
-    exit 1
-fi
-
 clang_format=`find_clang_format`
 if [[ $? -ne 0 ]]; then echo "clang-format ${clang_format_version} not found"; exit 0; fi
 echo "Found ${clang_format}"
@@ -39,14 +33,5 @@ file_list=`find "${src_dirs[@]}" -type f | grep -E "^.*\.(cxx|hxx)$"`
 for file in $file_list; do
     ${clang_format} ${clang_format_opts} ${file}
 done
-
-final_filelist=`git status | grep "modified"`
-if [[ -z $final_filelist ]]; then
-    echo "Code-style is pretty good :thumbsup:"
-    exit 0;
-fi
-
-echo "Read code-style guide again and apply those patches:"
-git diff
 
 exit 1
