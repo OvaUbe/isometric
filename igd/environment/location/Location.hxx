756ccc1256803c540239bf8cecfed2af5e03b8e0
ovaube <trevorismay@gmail.com>
2018-03-29 00:28:45 +0300

    ILocation: Advertise surface size.

--- igd/environment/location/Location.hxx
+++ igd/environment/location/Location.hxx
@@ -2,17 +2,32 @@
 
 #include <igd/environment/location/ILocation.hxx>
 
+#include <gum/ObservableValue.h>
+
 namespace igd {
 
 class Location : public virtual ILocation {
+    using ObservableSurfaceSize = gum::ObservableValue<SurfaceSize>;
+
+  private:
+    ISurfaceSizeRef _surfaceSize;
     ISurfaceMapRef _surfaceMap;
 
   public:
-    Location(const ISurfaceMapRef& surfaceMap)
-        : _surfaceMap(surfaceMap) {}
+    Location(const SurfaceSize& surfaceSize, const ISurfaceMapRef& surfaceMap)
+        : _surfaceSize(gum::make_shared_ref<ObservableSurfaceSize>(surfaceSize))
+        , _surfaceMap(surfaceMap) {}
+
+    IReadonlySurfaceSizeRef getReadonlySurfaceSize() const override {
+        return _surfaceSize;
+    }
 
     IReadonlySurfaceMapRef getReadonlySurfaceMap() const override {
         return _surfaceMap;
+    }
+
+    ISurfaceSizeRef getSurfaceSize() const override {
+        return _surfaceSize;
     }
 
     ISurfaceMapRef getSurfaceMap() const override {
