48381f955c2443bead360d9e6f20bb07443e0760
ovaube <trevorismay@gmail.com>
2018-03-30 23:19:29 +0300

    TypeRegistrator: Tweak style registration a bit.

--- ui/backend/TypeRegistrator.cxx
+++ ui/backend/TypeRegistrator.cxx
@@ -59,18 +59,32 @@ template <typename Type_>
 int registerSingleton(const gum::StringLiteral& typeName) {
     return registerSingleton<Type_>(typeName, typeName);
 }
+
+#define UI_DETAIL_DO_REGISTER_STYLE(Registrator_, Source_, Type_, Ns_, ChosenNs_)                                                                              \
+    do {                                                                                                                                                       \
+        Registrator_(Source_, Type_, "styles." Ns_ "." Type_);                                                                                                 \
+        if (Ns_ == ChosenNs_)                                                                                                                                  \
+            Registrator_(Source_, Type_, "styles." Type_);                                                                                                     \
+    } while (0)
+
+#define UI_DETAIL_REGISTER_STYLE(Source_, Type_, Ns_, ChosenNs_) UI_DETAIL_DO_REGISTER_STYLE(registerType, Source_, Type_, Ns_, ChosenNs_)
+
+#define UI_DETAIL_REGISTER_SINGLETON_STYLE(Source_, Type_, Ns_, ChosenNs_) UI_DETAIL_DO_REGISTER_STYLE(registerSingleton, Source_, Type_, Ns_, ChosenNs_)
 }
 
 TypeRegistrator::TypeRegistrator() {
+    const QString chosenStyle = "debug";
+
     registerSingleton("qrc:/qml/Backend.qml", "Backend");
 
-    registerSingleton("qrc:/qml/styles/debug/LocationStyle.qml", "LocationStyle", "styles.debug.LocationStyle");
-    registerType("qrc:/qml/styles/debug/SurfaceTile.qml", "SurfaceTile", "styles.debug.SurfaceTile");
-
-    registerSingleton("qrc:/qml/styles/debug/LocationStyle.qml", "LocationStyle", "styles.LocationStyle");
-    registerType("qrc:/qml/styles/debug/SurfaceTile.qml", "SurfaceTile", "styles.SurfaceTile");
+    UI_DETAIL_REGISTER_SINGLETON_STYLE("qrc:/qml/styles/debug/LocationStyle.qml", "LocationStyle", "debug", chosenStyle);
+    UI_DETAIL_REGISTER_STYLE("qrc:/qml/styles/debug/SurfaceTile.qml", "SurfaceTile", "debug", chosenStyle);
 
     registerType<ui::Launcher>("Launcher", "backend.Launcher");
     registerType<ui::Environment>("Environment", "backend.Environment");
 }
+
+#undef UI_DETAIL_DO_REGISTER_STYLE
+#undef UI_DETAIL_REGISTER_STYLE
+#undef UI_DETAIL_REGISTER_SINGLETON_STYLE
 }
