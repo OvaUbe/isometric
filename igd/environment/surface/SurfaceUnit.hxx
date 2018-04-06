26dd3119c85fe2072cc2977cff46200e4c74943d
ovaube <trevorismay@gmail.com>
2018-04-06 23:53:14 +0300

    ISurfaceUnit: Add getLevel.

--- igd/environment/surface/SurfaceUnit.hxx
+++ igd/environment/surface/SurfaceUnit.hxx
@@ -6,13 +6,19 @@ namespace igd {
 
 class SurfaceUnit : public virtual ISurfaceUnit {
     SurfaceMaterial _material;
+    gum::Optional<size_t> _level;
 
   public:
-    SurfaceUnit(SurfaceMaterial&& material)
-        : _material(std::move(material)) {}
+    SurfaceUnit(SurfaceMaterial&& material, const gum::Optional<size_t>& level = (size_t)0)
+        : _material(std::move(material))
+        , _level(level) {}
 
     const SurfaceMaterial& getMaterial() const override {
         return _material;
+    }
+
+    gum::Optional<size_t> getLevel() const override {
+        return _level;
     }
 };
 }
