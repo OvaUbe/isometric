10f52c56fcef7c88fa0c40a67846878420bc8998
subsinthe <subsinthe@gmail.com>
2018-05-12 17:37:08 +0300

    Add tiled map assets.

---
+++ assets/maps/scifi_platformTiles_32x32.tsx
@@ -0,0 +1,224 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="scifi_platformTiles_32x32" tilewidth="32" tileheight="32" tilecount="805" columns="35">
+ <image source="scifi-platform-tiles-32x32.png" width="1120" height="736"/>
+ <tile id="210">
+  <properties>
+   <property name="material" value="carbonTile"/>
+  </properties>
+ </tile>
+ <tile id="211">
+  <properties>
+   <property name="material" value="carbonTile"/>
+  </properties>
+ </tile>
+ <tile id="212">
+  <properties>
+   <property name="material" value="carbonTile"/>
+  </properties>
+ </tile>
+ <tile id="213">
+  <properties>
+   <property name="material" value="carbonTile"/>
+  </properties>
+ </tile>
+ <tile id="214">
+  <properties>
+   <property name="material" value="carbonTile"/>
+  </properties>
+ </tile>
+ <tile id="215">
+  <properties>
+   <property name="material" value="carbonTile"/>
+  </properties>
+ </tile>
+ <tile id="216">
+  <properties>
+   <property name="material" value="carbonTile"/>
+  </properties>
+ </tile>
+ <tile id="280">
+  <properties>
+   <property name="material" value="carbonWall"/>
+  </properties>
+ </tile>
+ <tile id="281">
+  <properties>
+   <property name="material" value="carbonWall"/>
+  </properties>
+ </tile>
+ <tile id="282">
+  <properties>
+   <property name="material" value="carbonWall"/>
+  </properties>
+ </tile>
+ <tile id="315">
+  <properties>
+   <property name="material" value="carbonWall"/>
+  </properties>
+ </tile>
+ <tile id="316">
+  <properties>
+   <property name="material" value="carbonWall"/>
+  </properties>
+ </tile>
+ <tile id="317">
+  <properties>
+   <property name="material" value="carbonWall"/>
+  </properties>
+ </tile>
+ <tile id="350">
+  <properties>
+   <property name="material" value="carbonWall"/>
+  </properties>
+ </tile>
+ <tile id="351">
+  <properties>
+   <property name="material" value="carbonWall"/>
+  </properties>
+ </tile>
+ <tile id="352">
+  <properties>
+   <property name="material" value="carbonWall"/>
+  </properties>
+ </tile>
+ <tile id="385">
+  <properties>
+   <property name="material" value="carbonWall"/>
+  </properties>
+ </tile>
+ <tile id="386">
+  <properties>
+   <property name="material" value="carbonWall"/>
+  </properties>
+ </tile>
+ <tile id="387">
+  <properties>
+   <property name="material" value="carbonWall"/>
+  </properties>
+ </tile>
+ <tile id="504">
+  <properties>
+   <property name="material" value="glass"/>
+  </properties>
+ </tile>
+ <tile id="505">
+  <properties>
+   <property name="material" value="glass"/>
+  </properties>
+ </tile>
+ <tile id="506">
+  <properties>
+   <property name="material" value="glass"/>
+  </properties>
+ </tile>
+ <tile id="507">
+  <properties>
+   <property name="material" value="glass"/>
+  </properties>
+ </tile>
+ <tile id="539">
+  <properties>
+   <property name="material" value="glass"/>
+  </properties>
+ </tile>
+ <tile id="540">
+  <properties>
+   <property name="material" value="glass"/>
+  </properties>
+ </tile>
+ <tile id="541">
+  <properties>
+   <property name="material" value="glass"/>
+  </properties>
+ </tile>
+ <tile id="542">
+  <properties>
+   <property name="material" value="glass"/>
+  </properties>
+ </tile>
+ <tile id="574">
+  <properties>
+   <property name="material" value="glass"/>
+  </properties>
+ </tile>
+ <tile id="575">
+  <properties>
+   <property name="material" value="glass"/>
+  </properties>
+ </tile>
+ <tile id="576">
+  <properties>
+   <property name="material" value="glass"/>
+  </properties>
+ </tile>
+ <tile id="577">
+  <properties>
+   <property name="material" value="glass"/>
+  </properties>
+ </tile>
+ <tile id="609">
+  <properties>
+   <property name="material" value="glass"/>
+  </properties>
+ </tile>
+ <tile id="610">
+  <properties>
+   <property name="material" value="glass"/>
+  </properties>
+ </tile>
+ <tile id="611">
+  <properties>
+   <property name="material" value="glass"/>
+  </properties>
+ </tile>
+ <tile id="612">
+  <properties>
+   <property name="material" value="glass"/>
+  </properties>
+ </tile>
+ <tile id="674">
+  <properties>
+   <property name="material" value="glass"/>
+  </properties>
+ </tile>
+ <tile id="675">
+  <properties>
+   <property name="material" value="glass"/>
+  </properties>
+ </tile>
+ <tile id="676">
+  <properties>
+   <property name="material" value="glass"/>
+  </properties>
+ </tile>
+ <tile id="709">
+  <properties>
+   <property name="material" value="glass"/>
+  </properties>
+ </tile>
+ <tile id="710">
+  <properties>
+   <property name="material" value="glass"/>
+  </properties>
+ </tile>
+ <tile id="711">
+  <properties>
+   <property name="material" value="glass"/>
+  </properties>
+ </tile>
+ <tile id="744">
+  <properties>
+   <property name="material" value="glass"/>
+  </properties>
+ </tile>
+ <tile id="745">
+  <properties>
+   <property name="material" value="glass"/>
+  </properties>
+ </tile>
+ <tile id="746">
+  <properties>
+   <property name="material" value="glass"/>
+  </properties>
+ </tile>
+</tileset>
